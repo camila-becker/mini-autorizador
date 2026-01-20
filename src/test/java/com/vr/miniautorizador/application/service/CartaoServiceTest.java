4d1c4b73fc2dc8097c7bd4ffbd0adb7b106c12bc
camila becker <camila.becker01@gmail.com>
2026-01-20 16:59:02 -0300

    Testes unitários do fluxo para obter saldo do cartão

--- src/test/java/com/vr/miniautorizador/application/service/CartaoServiceTest.java
+++ src/test/java/com/vr/miniautorizador/application/service/CartaoServiceTest.java
@@ -4,6 +4,7 @@ import com.vr.miniautorizador.application.usecase.CriarCartaoUseCase;
 import com.vr.miniautorizador.application.usecase.ResultadoCriarCartao;
 import com.vr.miniautorizador.domain.model.entities.Cartao;
 import com.vr.miniautorizador.interfaces.dto.CartaoRequest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -17,8 +18,7 @@ import java.util.stream.Stream;
 
 import static com.vr.miniautorizador.constants.TestesCartaoConstantes.NUMERO_CARTAO;
 import static com.vr.miniautorizador.constants.TestesCartaoConstantes.SENHA;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
@@ -32,7 +32,7 @@ class CartaoServiceTest {
     private CartaoService service;
 
     @ParameterizedTest
-    @MethodSource("cenarios")
+    @MethodSource("cenariosCriarCartao")
     void deveTestarCriacaoOuExistenciaDoCartao(Optional<Cartao> existente, Cartao retorno, Class<?> tipoEsperado,
                                                boolean deveSalvar) {
 
@@ -57,12 +57,33 @@ class CartaoServiceTest {
 
     }
 
-    static Stream<Arguments> cenarios() {
+    @Test
+    void deveRetornarOSaldoDoCartao() {
+
+        Cartao cartao = new Cartao(NUMERO_CARTAO, SENHA);
+
+        when(cartaoUseCase.buscarPorNumero(NUMERO_CARTAO)).thenReturn(Optional.of(cartao));
+
+        Optional<Cartao> resultado = service.obterSaldoCartao(NUMERO_CARTAO);
+
+        if(resultado.isPresent()) {
+            assertNotNull(resultado.get());
+            assertEquals(cartao.getSaldo(), resultado.get().getSaldo());
+        }
+
+        verify(cartaoUseCase).buscarPorNumero(anyString());
+
+    }
+
+    static Stream<Arguments> cenariosCriarCartao() {
+
         Cartao novoCartao = new Cartao(NUMERO_CARTAO, SENHA);
         Cartao existente = new Cartao(NUMERO_CARTAO, SENHA);
+
         return Stream.of(
                 Arguments.of(Optional.empty(), novoCartao, ResultadoCriarCartao.Criado.class, true),
                 Arguments.of(Optional.of(existente), existente, ResultadoCriarCartao.JaExistente.class, false)
         );
+
     }
 }
