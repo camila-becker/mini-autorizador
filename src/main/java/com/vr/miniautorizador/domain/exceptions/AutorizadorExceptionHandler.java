3e6ae0222020ce2b4f5f15076adf0d880ab63edf
camila becker <camila.becker01@gmail.com>
2026-01-20 19:49:54 -0300

    Add tratamento para erros de concorrência na transação

--- src/main/java/com/vr/miniautorizador/domain/exceptions/AutorizadorExceptionHandler.java
+++ src/main/java/com/vr/miniautorizador/domain/exceptions/AutorizadorExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.vr.miniautorizador.domain.exceptions;
 
+import com.vr.miniautorizador.domain.enums.ResultadoTransacaoEnum;
+import jakarta.persistence.OptimisticLockException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,5 +20,10 @@ public class AutorizadorExceptionHandler {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()) );
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler({OptimisticLockException.class, ObjectOptimisticLockingFailureException.class})
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT.value()).body(ResultadoTransacaoEnum.SALDO_INSUFICIENTE.name());
     }
 }
